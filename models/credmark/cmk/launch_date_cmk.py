6ad6d83182ceabf56e0e7323c5ffbce8ece72a4e
iwyrkore <iwyrkore@gmail.com>
2022-02-07 19:26:43 -0500

    Moved credmark files to credmark folder. Added vscode settings.

--- models/credmark/cmk/launch_date_cmk.py
+++ models/credmark/cmk/launch_date_cmk.py
@@ -4,6 +4,6 @@ from credmark import Model
 class LaunchDateCmk(Model):
 
     def run(self, data):
-        txs = self.context.ledger.get_transactions(["nonce"], {"min":"block_time"})
+        txs = self.context.ledger.get_transactions(
+            ["nonce"], {"min": "block_time"})
         return txs
-
