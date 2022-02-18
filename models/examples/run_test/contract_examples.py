ec37dd445ad9af47649839f5a08f2d2082caa1b2
Neil Zumwalde <neilzumwalde@gmail.com>
2022-02-18 14:44:56 -0600

    contract by address example

--- models/examples/run_test/contract_examples.py
+++ models/examples/run_test/contract_examples.py
@@ -19,3 +19,23 @@ class LoadContractByName(credmark.model.Model):
         for c in contracts:
             supplies.append(c.functions.totalSupply().call())
         return supplies
+
+
+@credmark.model.it(slug='load-contract-address',
+                   version='1.0',
+                   display_name='Contract Loading',
+                   description='Load the ABI of a Contract with its Name')
+class LoadContractByName(credmark.model.Model):
+
+    """
+    This Example Loads a Contract by it's name and returns all the addresses in our database
+    """
+
+    def run(self, input) -> dict:
+
+        contracts = self.context.contract_helper.get_contracts(
+            address="0x6d95d3ed3a6de47c384e667bf5f05c8e32a3e3a5")
+        supplies = []
+        for c in contracts:
+            supplies.append(c.functions.totalSupply().call())
+        return supplies
