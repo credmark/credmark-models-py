07f56082a252b5a6a6fa32ded581abc8e04656eb
Neil Zumwalde <neilzumwalde@gmail.com>
2022-02-27 14:40:57 -0600

    script to run all examples

---
+++ models/examples/run_all_examples.sh
@@ -0,0 +1,3 @@
+credmark-dev list
+credmark-dev run example-load-contract-by-name --input '{"contractName": "mutantmfers"}' --api_url=http://localhost:7000/v1/models/run -b 14234904
+credmark-dev run example-30-day-series --input '{"slug":"echo", "input":{"message":"hello world"}}' --api_url=http://localhost:7000/v1/models/run -b 14234904
