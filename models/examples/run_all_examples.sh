85e055a7afeb2be980a3bcfacff3cf26758b8bd5
Neil Zumwalde <neilzumwalde@gmail.com>
2022-02-27 19:16:31 -0600

    change echo to example-echo

--- models/examples/run_all_examples.sh
+++ models/examples/run_all_examples.sh
@@ -1,4 +1,9 @@
 credmark-dev list
+
+echo ""
+echo "ECHO Example:"
+echo ""
+credmark-dev run example-echo --input '{"message":"hello world"}'
 echo ""
 echo "BLOCKNUMBER Example:"
 echo ""
@@ -12,7 +17,7 @@ echo ""
 echo "Load Contract Examples:"
 echo ""
 credmark-dev run example-load-contract-by-name --input '{"contractName": "mutantmfers"}' --api_url=http://localhost:7000/v1/models/run -b 14234904
-credmark-dev run example-load-contract-by-address --input '{"address": "0x68CFb82Eacb9f198d508B514d898a403c449533E"}' --api_url=http://localhost:7000/v1/models/run -b 14234904
+credmark-dev run example-load-contract-by-address --input '{"address": "0xa8f8dd56e2352e726b51738889ef6ee938cca7b6"}' --api_url=http://localhost:7000/v1/models/run -b 14234904
 echo ""
 echo "Load Contract By Name Example:"
 echo ""
@@ -20,4 +25,4 @@ credmark-dev run example-load-contract-by-name --input '{"contractName": "mutant
 echo ""
 echo "Run 30 day Series Example:"
 echo ""
-credmark-dev run example-30-day-series --input '{"slug":"echo", "input":{"message":"hello world"}}' --api_url=http://localhost:7000/v1/models/run -b 14234904
+credmark-dev run example-30-day-series --input '{"slug":"example-echo", "input":{"message":"hello world"}}' --api_url=http://localhost:7000/v1/models/run -b 14234904
