b4d8e4d78ce278b4e4c49b2bc2f72e3a77277df4
Neil Zumwalde <neilzumwalde@gmail.com>
2022-02-28 12:03:38 -0600

    Total Supply for any ERC20 Working

--- models/credmark/token/erc20.py
+++ models/credmark/token/erc20.py
@@ -1,7 +1,37 @@
 import credmark.model
-from credmark.types.data import Address
+from credmark.types.data import Address, AddressStr
+from credmark.types.dto import DTO, DTOField
 
 min_erc20_abi = '[{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"type":"function"}]'
+
+
+class ERC20LookupDTO(DTO):
+    address: AddressStr = DTOField(None, description='Token Address')
+    symbol: str = DTOField(None, description='Token Symbol')
+
+
+class TokenAmountDto(DTO):
+    address: AddressStr = DTOField(None, description='Token Address')
+    amount: str = DTOField(None, description="The amount of a Token")
+    scaledAmount: float = DTOField(
+        None, description="The Amount of a Token, scaled by decimal Amount")
+
+
+@credmark.model.describe(slug='erc20-total-supply',
+                         version='1.0',
+                         display_name='ERC20 Total Supply',
+                         description='Get the Total Supply of an ERC20',
+                         input=Address,
+                         output=TokenAmountDto
+                         )
+class TotalSupply(credmark.model.Model):
+
+    def run(self, input: Address) -> TokenAmountDto:
+        contract = self.context.contracts.load_address(input.checksum)
+        totalSupply = contract.functions.totalSupply().call()
+        decimals = contract.functions.decimals().call()
+        scaledAmount = totalSupply / (10**decimals)
+        return TokenAmountDto(**{"address": input.address, "amount": totalSupply, "scaledAmount": scaledAmount})
 
 
 @credmark.model.describe(slug='erc20-balanceOf',
