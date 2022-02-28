cf0c4395c12bab3df65e5f8bf5d1ee811482e4eb
Neil Zumwalde <neilzumwalde@gmail.com>
2022-02-28 13:24:38 -0600

    file structuring

---
+++ models/credmark/algorithms/var.py
@@ -0,0 +1,24 @@
+import credmark.model
+
+
+@credmark.model.describe(slug='var',
+                         version='1.0',
+                         display_name='Value at Risk',
+                         description='Value at Risk')
+class Var(credmark.model.Model):
+
+    def run(self, input) -> dict:
+        """
+            Var takes in a portfolio object,
+            a list of prices per token into the past,
+            a price window,
+            and a worst case percentage.
+
+            It calculates the usd value of the portfolio for each of the blockstamps/timestamps.
+            It then calculates the change in value over the window period for each timestamp,
+            it returns the one that hits the input percentage.
+
+        """
+        result = {'value': 'not yet implemented'}
+
+        return result
