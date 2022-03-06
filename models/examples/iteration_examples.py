3826a1dea5a0a413e4d9b4b47d61b45671a7ec33
Neil Zumwalde <neilzumwalde@gmail.com>
2022-03-06 15:12:31 -0600

    iterator example and price

---
+++ models/examples/iteration_examples.py
@@ -0,0 +1,23 @@
+import credmark.model
+from credmark.types import Wallet, Wallets
+
+
+@credmark.model.describe(slug='example-iteration',
+                         version='1.0',
+                         display_name='Echo',
+                         description="A test model to echo the message property sent in input.")
+class EchoModel(credmark.model.Model):
+    """
+    This test simply echos back the input.
+    The DTO message field defines a default value so that is
+    used if no input is sent.
+    """
+
+    def run(self, input) -> dict:
+        wallets = Wallets(wallets=[Wallet(address="0x68cfb82eacb9f198d508b514d898a403c449533e"),
+                                   Wallet(address="0x59e1f901b5c33ff6fae15b61684ebf17cca7b9b3")])
+        for w in wallets:
+            print(w)
+
+        print(wallets[1])
+        return {}
