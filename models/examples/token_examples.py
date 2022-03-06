cbe4757405340edc4ad5dab46383ca74bcadd473
Neil Zumwalde <neilzumwalde@gmail.com>
2022-03-06 12:59:35 -0600

    Example Token Loading

---
+++ models/examples/token_examples.py
@@ -0,0 +1,19 @@
+from pydoc import describe
+import credmark.model
+from credmark.types.data.token import Token
+
+
+@credmark.model.describe(slug='example-token-loading', version='1.0')
+class ExampleTokenLoading(credmark.model.Model):
+    def run(self, input) -> dict:
+        res = {}
+        res['loadedFromAddress'] = self.context.run_model(
+            'example-token-input', {"address": "0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9"})
+        res['loadedFromSymbol'] = self.context.run_model('example-token-input', {"symbol": "CMK"})
+        return res
+
+
+@credmark.model.describe(slug='example-token-input', version='1.0', input=Token)
+class ExampleTokenInput(credmark.model.Model):
+    def run(self, input: Token) -> Token:
+        return input
