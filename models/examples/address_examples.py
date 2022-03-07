06984f8e81f82b379c8b79d8ed1bb6071fafbf80
Neil Zumwalde <neilzumwalde@gmail.com>
2022-03-07 12:40:49 -0600

    making examples more verbose

--- models/examples/address_examples.py
+++ models/examples/address_examples.py
@@ -1,5 +1,6 @@
 import credmark.model
 from credmark.types import Address, Wallet
+from credmark.types.dto import DTO
 
 
 @credmark.model.describe(slug='example-address',
@@ -12,21 +13,35 @@ from credmark.types import Address, Wallet
 class AddressExample(credmark.model.Model):
     def run(self, input: Wallet) -> Wallet:
         """
-            This model demonstrates how to take in an address as an input, and output an address as an output.
+            This model demonstrates how to take in an address as an input, 
+            and output an address as an output. 
+
+            We use the wallet class in order to format them with any wallet
         """
         result = input
         return result
 
 
+class AddressTransformsExampleOutput(DTO):
+    inputAddress: Address
+    checksumAddress: str
+    lowerAddress: str
+
+
 @credmark.model.describe(slug='example-address-transforms',
                          version='1.0',
                          display_name='(Example) Address Transforms',
-                         description='Input an address and output the same address',
+                         description='Input an address and output transformations we can make to that address',
                          developer='Credmark',
-                         input=Wallet)
+                         input=Wallet,
+                         output=AddressTransformsExampleOutput)
 class AddressTransformsExample(credmark.model.Model):
-    def run(self, input: Wallet) -> dict:
+    def run(self, input: Wallet) -> AddressTransformsExampleOutput:
         """
             This model demonstrates how to take in an address as an input, and output an address as an output.
         """
-        return {"inputAddress": input.address, "checksumAddress": input.address.checksum, "lowerAddress": input.address.lower()}
+        return AddressTransformsExampleOutput(
+            inputAddress=input.address,
+            checksumAddress=input.address.checksum,
+            lowerAddress=input.address.lower()
+        )
--- models/examples/blocknumber_examples.py
+++ models/examples/blocknumber_examples.py
@@ -1,33 +1,47 @@
 import credmark.model
 from credmark.types import BlockNumber
 from credmark.types.data.block_number import BlockNumberOutOfRangeException
+from credmark.types.dto import DTO
+
+
+class BlockNumberTransformExampleOutput(DTO):
+    blockNumber: int
+    blockTime: int
+    blockDatestring: str
+    tenThousandBlocksAgo: int
+    tenThousandBlocksAgoTimestamp: int
+    tenThousandBlocksAgoDatestring: str
 
 
 @credmark.model.describe(slug='example-blocktime',
                          version='1.0',
                          display_name='(Example) BlockNumber',
-                         description='The Time of the block of the execution context')
-class BlockTimeExample(credmark.model.Model):
+                         description='The Time of the block of the execution context',
+                         output=BlockNumberTransformExampleOutput)
+class BlockNumberTransformExample(credmark.model.Model):
 
     """
-    This example returns the timestamp of the context block, contextualized.
+    This example returns information about the current block, attempts
+    to look at a future block, 
+    and offers information about a previous block.
     """
 
-    def run(self, input) -> dict:
+    def run(self, input) -> BlockNumberTransformExampleOutput:
+
         block = self.context.block_number
 
         try:
             block = block + 1
         except BlockNumberOutOfRangeException:
-            print("I can't look into the future, so don't try.")
+            print("I can't look into the future, looking at the next block was attempted.")
 
         ten_thousand_blocks_ago = self.context.block_number - 10000
 
-        return {
-            "blockNumber": block,
-            "blockTime": block.timestamp,
-            "blockDatestring": block.datestring,
-            "tenThousandBlocksAgo": ten_thousand_blocks_ago,
-            "tenThousandBlocksAgoTimestamp": ten_thousand_blocks_ago.timestamp,
-            "tenThousandBlocksAgoDatestring": ten_thousand_blocks_ago.datestring
-        }
+        return BlockNumberTransformExampleOutput(
+            blockNumber=block,
+            blockTime=block.timestamp,
+            blockDatestring=block.datestring,
+            tenThousandBlocksAgo=ten_thousand_blocks_ago,
+            tenThousandBlocksAgoTimestamp=ten_thousand_blocks_ago.timestamp,
+            tenThousandBlocksAgoDatestring=ten_thousand_blocks_ago.datestring
+        )
