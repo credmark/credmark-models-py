3e5ee5872ddc48abc774da4a0a7508eb0f30a7f4
kunlun <214410+leafyoung@users.noreply.github.com>
2022-03-09 16:28:56 +0800

    add model version change to use Address instead of baremetal

--- models/credmark/protocols/aave/aave_v2.py
+++ models/credmark/protocols/aave/aave_v2.py
@@ -147,6 +147,16 @@ class AaveV2GetTokenAsset(credmark.model.Model):
                          input=Token,
                          output=BlockSeries)
 class AaveV2GetTokenAssetHistorical(credmark.model.Model):
+<<<<<<< HEAD
     def run(self, input: Token) -> BlockSeries:
         return self.context.historical.run_model_historical(
             'aave-token-asset', model_input=input, window='5 days', interval='1 day')
+=======
+    def run(self, input: Token) -> dict:
+        output = {}
+        historical_data = self.context.historical.run_model_historical(
+            'aave-token-asset', model_input=input, window='5 days', interval='1 day', model_version='1.0')
+
+        output['historical_data'] = historical_data
+        return output
+>>>>>>> 55d8937 (add model version)
