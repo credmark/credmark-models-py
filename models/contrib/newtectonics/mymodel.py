0654b05658b98b128b32d5d52084420dad3d6d9b
Neil Zumwalde <neilzumwalde@gmail.com>
2022-03-12 17:02:12 -0600

    token example and contrib example

--- models/contrib/newtectonics/mymodel.py
+++ models/contrib/newtectonics/mymodel.py
@@ -2,10 +2,16 @@ import credmark.model
 import credmark.types
 
 
-@credmark.model.describe(slug='contrib.neils-model', version='1.0')
+@credmark.model.describe(
+    slug='contrib.neilz',
+    version='1.0',
+    developer='neilz.eth',
+    input=None,
+    output=dict
+)
 class MyModel(credmark.model.Model):
     def run(self, input):
         return {
-            "founder": "Neil",
+            "credmarkFounder": "Neil",
             "message": "You are a modeler. Thank you modeler."
         }
--- models/examples/token_examples.py
+++ models/examples/token_examples.py
@@ -11,22 +11,21 @@ from typing import (
 @credmark.model.describe(slug='example.token-loading', version='1.0')
 class ExampleTokenLoading(credmark.model.Model):
     def run(self, input) -> Dict[str, Union[Token, float]]:
+
         res = {}
-
-        res['loadedFromAddress'] = self.context.run_model(
+        loaded_from_address = self.context.run_model(
             'example.token-input',
-            Token(address='0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9'))
-
-        # self.context.run_model('example.token-input',Token(address='0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9'))
-
+            Token(address='0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9'), return_type=Token)
+        res['loadedFromAddress'] = loaded_from_address
         res['loadedFromSymbol'] = self.context.run_model('example.token-input',
                                                          Token(symbol='CMK'))
-
-        res['loadedFromSymbolPrice'] = Token(symbol='CMK').price_usd
+        # res['loadedFromSymbolPrice'] = Token(symbol='CMK').price_usd
+        res['loadedNativeToken'] = Token.native_token()
+        #res['loadedNativePrice'] = res['loadedNativeToken'].price_usd
         return res
 
 
-@credmark.model.describe(slug='example.token-input', version='1.0', input=Token)
+@credmark.model.describe(slug='example.token-input', version='1.0', input=Token, output=Token)
 class ExampleTokenInput(credmark.model.Model):
     def run(self, input: Token) -> Token:
         return input
