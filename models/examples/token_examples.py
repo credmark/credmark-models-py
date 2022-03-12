2e4f16f77df70facf9e444eb21c38ac27b059354
Neil Zumwalde <neilzumwalde@gmail.com>
2022-03-12 17:32:04 -0600

    Token examples are up to date

--- models/examples/token_examples.py
+++ models/examples/token_examples.py
@@ -1,6 +1,7 @@
-from pydoc import describe
+
 import credmark.model
-from credmark.types import Token
+from credmark.types import Token, Address
+from credmark.types.dto import DTO
 
 from typing import (
     Dict,
@@ -8,24 +9,31 @@ from typing import (
 )
 
 
-@credmark.model.describe(slug='example.token-loading', version='1.0')
+class ExampleTokenLoadingOutput(DTO):
+    loadedFromAddress: Token
+    loadedFromSymbol: Token
+    loadedNativeToken: Token
+    loadedFromSymbolPrice: float
+    loadedFromSymbolTotalSupply: float
+    loadedFromSymbolBalanceOf: float
+    loadedFromSymbolBalanceOfWei: int
+
+
+@credmark.model.describe(slug='example.token-loading',
+                         version='1.0',
+                         input=None,
+                         developer='credmark',
+                         output=ExampleTokenLoadingOutput)
 class ExampleTokenLoading(credmark.model.Model):
-    def run(self, input) -> Dict[str, Union[Token, float]]:
-
-        res = {}
-        loaded_from_address = self.context.run_model(
-            'example.token-input',
-            Token(address='0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9'), return_type=Token)
-        res['loadedFromAddress'] = loaded_from_address
-        res['loadedFromSymbol'] = self.context.run_model('example.token-input',
-                                                         Token(symbol='CMK'))
-        # res['loadedFromSymbolPrice'] = Token(symbol='CMK').price_usd
-        res['loadedNativeToken'] = Token.native_token()
-        #res['loadedNativePrice'] = res['loadedNativeToken'].price_usd
-        return res
-
-
-@credmark.model.describe(slug='example.token-input', version='1.0', input=Token, output=Token)
-class ExampleTokenInput(credmark.model.Model):
-    def run(self, input: Token) -> Token:
-        return input
+    def run(self, input) -> ExampleTokenLoadingOutput:
+        cmk = Token(symbol='CMK')
+        cmk_holder = Address("0xF6dBFf8433b643bc08cB53BeD6C535c8a57AC912")
+        return ExampleTokenLoadingOutput(
+            loadedFromAddress=Token(address='0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9'),
+            loadedFromSymbol=cmk,
+            loadedNativeToken=Token.native_token(),
+            loadedFromSymbolPrice=cmk.price_usd,
+            loadedFromSymbolTotalSupply=cmk.total_supply().scaled,
+            loadedFromSymbolBalanceOf=cmk.balance_of(cmk_holder).scaled,
+            loadedFromSymbolBalanceOfWei=cmk.balance_of(cmk_holder)
+        )
