3cc1b05305b66ccd9a4272f606ff7b1ba65509c8
Neil Zumwalde <neilzumwalde@gmail.com>
2022-03-14 14:18:42 -0500

    contrib example

--- models/contrib/neilz/example-contribution.py
+++ models/contrib/neilz/example-contribution.py
@@ -4,6 +4,8 @@ import credmark.types
 
 @credmark.model.describe(
     slug='contrib.neilz',
+    display_name='An example of a contrib model',
+    description="This model exists simply as an example of how and where to contribute a model to the Credmark framework",
     version='1.0',
     developer='neilz.eth',
     input=None,
