69f2487962cc2417684b2d3c7a135a9d7dea893f
Neil Zumwalde <neilzumwalde@gmail.com>
2022-03-25 11:24:03 -0500

    example.block-number

--- models/examples/blocknumber_examples.py
+++ models/examples/blocknumber_examples.py
@@ -9,6 +9,7 @@ from credmark.model.errors import ModelRunError
                          version='1.0',
                          display_name='(Example) BlockNumber',
                          description='This example shows the capabilities on the BlockNumber class',
+                         input=None,
                          output=dict)
 class BlockNumberTransformExample(credmark.model.Model):
 
@@ -24,28 +25,28 @@ class BlockNumberTransformExample(credmark.model.Model):
         self.logger.info(
             f"block_number.timestamp : {block_number.timestamp}")
         self.logger.info(
-            f"block_number.datestring : {block_number.datestring}")
+            f"block_number.timestamp_datetime : {block_number.timestamp_datetime}")
         self.logger.info(
-            f"block_number - 100 + 50 : {(block_number - 100) + 50}")
+            f"(block_number - 1000) : {(block_number - 1000)}")
         self.logger.info(
-            f"block_number.from_datetime(block_number.timestamp - 3600) : \
-                {block_number.from_timestamp(block_number.timestamp - 3600)}")
+            f"(block_number - 1000).timestamp_datetime : {(block_number - 1000).timestamp_datetime}")
         self.logger.info(
-            f"BlockNumber.from_datetime(block_number.timestamp - 3600) : \
-                {BlockNumber.from_timestamp(block_number.timestamp - 3600)}")
+            f"block_number.from_datetime(block_number.timestamp - 3600): {block_number.from_timestamp(block_number.timestamp - 3600)}")
+        self.logger.info(
+            f"BlockNumber.from_datetime(block_number.timestamp - 3600): {BlockNumber.from_timestamp(block_number.timestamp - 3600)}")
 
         try:
-            block_number + 10
-            raise ModelRunError(
-                message="BlockNumbers cannot exceed the current context.block_number, an exception was NOT caught, and the example has FAILED")
-        except BlockNumberOutOfRangeError:
             """
                 NOTE: THIS IS FOR DEMONSTRATION ONLY.
                 You should NOT catch BlockNumberOutOfRangeError or
                 other ModelRunErrors in your models!
             """
-
+            block_number + 1000000
+            raise ModelRunError(
+                message="BlockNumbers cannot exceed the current context.block_number, an exception was NOT caught, and the example has FAILED")
+        except BlockNumberOutOfRangeError as _e:
+            self.logger.info(_e)
             self.logger.info(
-                "BlockNumbers cannot exceed the current context.block_number, an exception was caught successfully")
+                "Attempting to create a BlockNumber object higher than the current context's block_number raises BlockNumberOutOfRangeError")
 
         return dict(block_number=block_number)
