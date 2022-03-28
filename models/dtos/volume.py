d87bf87fe5142ccbe2b18b1fd8d2dffc1e816c6f
iwyrkore <iwyrkore@gmail.com>
2022-03-28 15:22:56 -0400

    Removed unused import.

--- models/dtos/volume.py
+++ models/dtos/volume.py
@@ -1,12 +1,14 @@
 from typing import List
-from credmark.types import Token, Contract
+from credmark.types import Token
 from credmark.dto import DTO, IterableListGenericDTO
+
 
 class TokenTradingVolume(DTO):
     token: Token
     sellAmount: int
     buyAmount: int
 
+
 class TradingVolume(IterableListGenericDTO[TokenTradingVolume]):
     tokenVolumes: List[TokenTradingVolume]
-    _iterator:str = "tokenVolumes"
+    _iterator: str = "tokenVolumes"
