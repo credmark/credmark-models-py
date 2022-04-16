502273cb25d10555430c1755ab03121707d325eb
Neil Zumwalde <neilzumwalde@gmail.com>
2022-04-16 10:20:57 -0500

    accounts

--- models/examples/account_examples.py
+++ models/examples/account_examples.py
@@ -1,5 +1,7 @@
+# pylint: disable=line-too-long
 from credmark.cmf.model import Model, EmptyInput
 from credmark.cmf.types import Account, Accounts, Address
+from .example_dtos import ExampleModelOutput
 
 
 @Model.describe(
@@ -9,26 +11,36 @@ from credmark.cmf.types import Account, Accounts, Address
     description='This model gives examples of the functionality available on the Account class',
     developer='Credmark',
     input=EmptyInput,
-    output=dict)
+    output=ExampleModelOutput)
 class ExampleAccount(Model):
-    def run(self, input) -> dict:
+    def run(self, input) -> ExampleModelOutput:
         """
-            This model demonstrates the functionality of the Account class
+            This model demonstrates the functionality of the Account class.
         """
+
+        example = ExampleModelOutput(
+            github_url="https://github.com/credmark/credmark-models-py/blob/main/models/examples/account_examples.py",
+            documentation_url="https://developer-docs.credmark.com/en/latest/reference/credmark.cmf.types.account.html"
+            )
+
         account_1 = Account(address=Address('0xeB2629a2734e272Bcc07BDA959863f316F4bD4Cf'))
-        self.logger.info(
-            "Account is the base Data Transfer Object for classes Contract and Token.")
-        self.logger.info(
-            "You initialize an Account with an address.")
-        self.logger.info(
-            f"Account(address='0xeB2629a2734e272Bcc07BDA959863f316F4bD4Cf') : {account_1.dict()}")
         account_2 = Account(address=Address('0x2F50D538606Fa9EDD2B11E2446BEb18C9D5846bB'))
-        self.logger.info(
+        accounts = Accounts(accounts=[account_1, account_2])
+
+        example.log(
+            "Account is the base Data Transfer Object for classes Contract and Token.")
+
+        example.log(
+            "You initialize an Account with an address.")
+
+        example.log(
+            f"Account(address='0xeB2629a2734e272Bcc07BDA959863f316F4bD4Cf') : {account_1.dict()}")
+
+        example.log(
             "There is also an iterable Accounts Data Transfer Object,"
             " initialized with a list of accounts")
-        accounts = Accounts(accounts=[account_1, account_2])
-        self.logger.info(
+        
+        example.log(
             f"Accounts(accounts=[account_1, account_2]) : {accounts.dict()}")
 
-        return {"message": "see https://github.com/credmark/credmark-models-py/blob/main/model"
-                "s/examples/account_examples.py for examples of Account usage"}
+        return example
