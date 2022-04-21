3dc60c85c54e0868c9f07b45c800e800424d44bd
Vineet <vineet@altercode.xyz>
2022-04-21 18:51:17 +0530

    Added io, error log helpers in example output dto

--- models/examples/example_dtos.py
+++ models/examples/example_dtos.py
@@ -1,16 +1,27 @@
+import logging
+from typing import Union
+
+from credmark.cmf.model.errors import ModelBaseError
 from credmark.dto import DTO
 
-import logging
 
 class ExampleModelOutput(DTO):
-    github_url:str
-    documentation_url:str
-    message:str = ""
+    github_url: str
+    documentation_url: str
+    message: str = ""
 
-    def __init__(self, **data) -> None:
-        super().__init__(**data)
-
-    def log(self, message):
+    def log(self, message: str):
         logger = logging.getLogger('credmark.cmf.model.example')
         logger.info(message)
         self.message += '\n' + message
+
+    def log_io(self, input: str, output):
+        self.log(f"{input} : {output}")
+
+    def log_error(self, error: Union[str, Exception]):
+        if isinstance(error, str):
+            self.log(error)
+        elif isinstance(error, ModelBaseError):
+            self.log(error.data.message)
+        else:
+            self.log(str(error))
