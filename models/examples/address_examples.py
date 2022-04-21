d7af629ea227ba2e204a0b6db9b6283dda10aaee
Vineet <vineet@altercode.xyz>
2022-04-21 23:36:57 +0530

    Updated address example

--- models/examples/address_examples.py
+++ models/examples/address_examples.py
@@ -1,5 +1,12 @@
-from credmark.cmf.model import Model, EmptyInput
+from random import choice
+from credmark.cmf.model import Model
 from credmark.cmf.types import Address
+from credmark.dto import DTO
+from models.examples.example_dtos import ExampleModelOutput
+
+
+class _AddressInput(DTO):
+    address: Address = Address("0xeB2629a2734e272Bcc07BDA959863f316F4bD4Cf")
 
 
 @Model.describe(
@@ -8,41 +15,46 @@ from credmark.cmf.types import Address
     display_name='Address Usage Examples',
     description='This model gives examples of the functionality available on the Address class',
     developer='Credmark',
-    input=EmptyInput,
-    output=dict)
+    input=_AddressInput,
+    output=ExampleModelOutput)
 class ExampleAddress(Model):
-    def run(self, input) -> dict:
-        """
-            This model demonstrates the functionality of the Address class
-        """
+    def run(self, input: _AddressInput) -> ExampleModelOutput:
+        output = ExampleModelOutput(
+            github_url="https://github.com/credmark/credmark-models-py/blob/main/models/examples/address_examples.py",
+            documentation_url="https://developer-docs.credmark.com/en/latest/reference/credmark.cmf.types.address.Address.html")
 
-        address = Address("0xeB2629a2734e272Bcc07BDA959863f316F4bD4Cf")
+        output.log("This model demonstrates the functionality of the Address class")
 
-        self.logger.info(
-            f"Address automatically normalizes to lowercase : {address}")
-        self.logger.info(
-            f"Address('0xeb2629a2734e272bcc07bda959863f316f4bd4cf').checksum : {address.checksum}")
-        self.logger.info(
-            "Address equality exists for string and Address, and is case insensitive")
-        self.logger.info(
-            f"address == '0xeB2629a2734e272Bcc07BDA959863f316F4bD4Cf' : "
-            f"{address == '0xeB2629a2734e272Bcc07BDA959863f316F4bD4Cf'}")
-        self.logger.info(
-            f"Address equality for strings is case-insensitive : "
-            f"{address == '0xeb2629a2734e272bcc07bda959863f316f4bd4cf'}")
-        self.logger.info(
-            f"You can hash the address : {hash(address)}")
+        address = input.address
 
-        self.logger.info(
+        output.log("Address automatically normalizes to lowercase")
+        output.log_io(input="address", output=address)
+
+        output.log_io(input=f"Address('{address}').checksum", output=address.checksum)
+
+        output.log("Address equality exists for string and Address")
+        output.log_io(input=f"address == '{address}'",
+                      output=address == str(address))
+        output.log_io(input=f"address == Address('{address}')",
+                      output=address == Address(str(address)))
+
+        random_case_address = '0x'+(''.join(choice((str.upper, str.lower))(char) for char in address[2:]))
+        output.log("Address equality is case-insensitive")
+        output.log_io(input=f"{random_case_address} == {address}",
+                      output=random_case_address == address)
+
+        output.log("You can hash the address")
+        output.log_io(input="hash(address)", output=hash(address))
+
+        output.log(
             f"Address.null() returns the NULL Address : {Address.null()}")
-        self.logger.info(
-            "Address.valid() is whether a string is a valid address format")
-        self.logger.info(
-            f"Address.valid('0xeB2629a2734e272Bcc07BDA959863f316F4bD4Cf') : "
-            f"{Address.valid('0xeB2629a2734e272Bcc07BDA959863f316F4bD4Cf')}")
-        self.logger.info(
+        output.log(
+            "Address.valid() checks whether a string is a valid address format")
+        output.log_io(input=f"Address.valid('{address}')",
+                      output=Address.valid(address))
+
+        output.log(
             f"Address.valid('0xThIsIsNoTaVaLiDaDdReSsItSGaRbaGeLeTtErSs') : "
             f"{Address.valid('0xThIsIsNoTaVaLiDaDdReSsItSGaRbaGeLeTtErSs')}")
 
-        return {"message": "see https://github.com/credmark/credmark-models-py"
-                "/blob/main/models/examples/address_examples.py for examples of Address usage"}
+        return output
