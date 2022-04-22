bcb1acf352382794b6d816a008ba0c228ac0fdcd
Vineet <vineet@altercode.xyz>
2022-04-22 12:23:22 +0530

    Updated contract example

--- models/examples/contract_examples.py
+++ models/examples/contract_examples.py
@@ -1,5 +1,8 @@
 from credmark.cmf.model import Model, EmptyInput
 from credmark.cmf.types import Contract
+from models.examples.example_dtos import ExampleModelOutput
+
+from models.tmp_abi_lookup import CMK_ABI, CMK_ADDRESS
 
 
 @Model.describe(
@@ -11,15 +14,26 @@ from credmark.cmf.types import Contract
     input=EmptyInput,
     output=dict)
 class ExampleAddress(Model):
-    def run(self, input) -> dict:
-        """
-            This model demonstrates the functionality of the Address class
-        """
-        # curve.fi gauge controller
-        contract = Contract(address='0x2F50D538606Fa9EDD2B11E2446BEb18C9D5846bB')
+    def run(self, _) -> ExampleModelOutput:
+        output = ExampleModelOutput(
+            github_url="https://github.com/credmark/credmark-models-py/blob/main/models/examples/contract_examples.py",
+            documentation_url="https://developer-docs.credmark.com/en/latest/reference/credmark.cmf.types.contract.Contract.html")
 
-        self.logger.info(
-            "Contract is a subclass of Account, and is itialized with an address.")
-        self.logger.info(
-            f"Contract(address='0x2F50D538606Fa9EDD2B11E2446BEb18C9D5846bB') : {contract.dict()}")
-        return {"message": "see https://github.com/credmark/credmark-models-py/blob/main/models/examples/contract_examples.py for examples of Contract usage"}  # pylint: disable=line-too-long
+        output.log("This model demonstrates the functionality of the Contract class")
+
+        output.log("Contract is a subclass of Account, and is initialized with an address.")
+        contract = Contract(address=CMK_ADDRESS)
+        output.log_io(input=f"Contract(address='{CMK_ADDRESS}')", output=contract.dict())
+
+        output.log("You can interact with contract functions")
+        output.log_io(input="contract.functions.name().call()",
+                      output=contract.functions.name().call())
+        output.log_io(input="contract.functions.symbol().call()",
+                      output=contract.functions.symbol().call())
+        output.log_io(input="contract.functions.decimals().call()",
+                      output=contract.functions.decimals().call())
+        output.log("You can also pass parameters to contract functions")
+        output.log_io(input="contract.functions.balanceOf('0xCbF507C87f19B58fB719B65697Fb7fA84D682aA9').call()",
+                      output=contract.functions.balanceOf('0xCbF507C87f19B58fB719B65697Fb7fA84D682aA9').call())
+
+        return output
