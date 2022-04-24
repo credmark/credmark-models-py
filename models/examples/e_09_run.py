f730e7369976a3c2628fde78d70bcc214304711c
Vineet <vineet@altercode.xyz>
2022-04-24 22:40:58 +0530

    Updated model run example

--- models/examples/e_09_run.py
+++ models/examples/e_09_run.py
@@ -1,38 +1,43 @@
 from credmark.cmf.model import Model
-
-from credmark.dto import DTO
-
-
-class RunTestIn(DTO):
-    model: str
-    input: dict
+from credmark.dto import EmptyInput
+from models.dtos.example import ExampleModelOutput
 
 
-class RunTestOut(DTO):
-    model: str
-    output: dict
+class RunExampleOutput(ExampleModelOutput):
+    model_slug: str
+    model_output: dict
 
 
 @Model.describe(
-    slug='example.run-test',
+    slug='example.model-run',
     version='1.0',
     display_name='Runner test model',
     description='Test model runs another model specified with \'model\' in input.',
     developer='Credmark',
-    input=RunTestIn,
-    output=RunTestOut)
+    input=EmptyInput,
+    output=RunExampleOutput)
 class RunnerTestModel(Model):
-    """A test model that runs another model that's specified
-    in the input. For example: {"model":"example.echo"}
-    """
+    def run(self, _) -> RunExampleOutput:
+        output = RunExampleOutput(
+            github_url="https://github.com/credmark/credmark-models-py/blob/main/models/examples/e_09_run.py",
+            documentation_url="https://developer-docs.credmark.com/en/latest/components.html#context-run-model",
+            model_slug="example.model",
+            model_output={}
+        )
 
-    def run(self, input: RunTestIn) -> RunTestOut:
+        output.log("This model demonstrates how you run a model within a model.")
+        output.log("Models are exposed on context.models by their slug (with any \"-\" (hyphens) in "
+                   "the slug replaced with \"_\" (underscores)) and can be called like a function, "
+                   "passing the input as a DTO or dict or as standard keyword args (kwargs).")
+        output.log_io(input="self.context.models.example.model({\"message\": \"Hello from model-run example\"})",
+                      output="")
+        output.log("Or you can use the run_model method of the context.")
+        output.log_io(input="self.context.run_model(slug=\"example.model\", input={\"message\": \"Hello from model-run example\"})",
+                      output="")
+        output.log("--------------------------------------------------------------------------------")
+        model_output = self.context.run_model(slug='example.model', input={"message": "Hello from model-run example"})
+        output.log("--------------------------------------------------------------------------------")
+        output.log_io(input="", output=model_output)
 
-        model = input.model
-
-        if model:
-            res = self.context.run_model(model, input.input)
-        else:
-            res = {'result': 'No model specified'}
-
-        return RunTestOut(model=model, output=res)
+        output.model_output = model_output
+        return output
