092775d7660ab5d34b11588e82fcbb6e9a3c9061
Vineet <vineet@altercode.xyz>
2022-04-24 22:41:36 +0530

    Updated example class in dto example

--- models/examples/e_02_dto.py
+++ models/examples/e_02_dto.py
@@ -21,34 +21,37 @@ class DtoExampleModel(Model):
 
         class Token(DTO):
             symbol: str
-            total_supply: int = 1000
+            decimals: int = 18
 
-        output.log("""
+        output.log_io(input="""
 class Token(DTO):
     symbol: str
-    total_supply: int = 1000""")
+    total_supply: int = 1000""",
+                      output="")
 
-        token = Token(symbol='CMK', total_supply=1000)
-        output.log_io(input="Token(symbol='CMK', total_supply = 1000)", output=token)
+        token = Token(symbol='CMK')
+        output.log_io(input="Token(symbol='CMK')", output=token)
         output.log_io(input="token.dict()", output=token.dict())
         output.log_io(input="token.schema_json()", output=token.schema_json())
 
         output.log("To declare a field as required, you may declare it using just"
                    " an annotation, or you may use an ellipsis (...) as the value:")
-        output.log("""
+        output.log_io(input="""
 class Token(DTO):
     a: int
     b: int = ...
-    c: int = DTOField(...)""")
+    c: int = DTOField(...)""",
+                      output="")
 
         output.log("You can use default_factory to declare field with dynamic value")
-        output.log("""
+        output.log_io(input="""
 class Token(DTO):
     uid: UUID = DTOField(default_factory=uuid4)
-    updated: datetime = DTOField(default_factory=datetime.utcnow)""")
+    updated: datetime = DTOField(default_factory=datetime.utcnow)""",
+                      output="")
 
         output.log("For custom validation, use the validator decorator.")
-        output.log("""
+        output.log_io(input="""
 class Token(DTO):
     symbol: str
 
@@ -56,7 +59,8 @@ class Token(DTO):
     def symbol_must_not_contain_space(cls, v):
         if ' ' in v:
             raise ValueError('must not contain a space')
-        return v""")
+        return v""",
+                      output="")
 
         return output
 
@@ -95,7 +99,7 @@ class TestModel(Model):
                 input=Portfolio,
                 output=PortfolioSummary)
 class TestModel2(Model):
-    def run(self, _) -> PortfolioSummary:
+    def run(self, input: Portfolio) -> PortfolioSummary:
         output = PortfolioSummary(
             github_url="https://github.com/credmark/credmark-models-py/blob/main/models/examples/dto_examples.py",
             documentation_url="https://developer-docs.credmark.com/en/latest/components.html#data-transfer-object-dto",
