d79ebcb3190ed53afa97fe32ce0c69d18aaeca50
Vineet <vineet@altercode.xyz>
2022-04-24 22:40:48 +0530

    Updated model example

--- models/examples/e_01_model.py
+++ models/examples/e_01_model.py
@@ -1,9 +1,14 @@
 from credmark.cmf.model import Model
 from credmark.dto import DTO, DTOField
+from models.dtos.example import ExampleModelOutput
 
 
-class EchoDto(DTO):
+class EchoExampleInput(DTO):
     message: str = DTOField('Hello', description='A message')
+
+
+class EchoExampleOutput(ExampleModelOutput):
+    echo: str
 
 
 @Model.describe(slug='example.model',
@@ -11,8 +16,19 @@ class EchoDto(DTO):
                 display_name='Example (Model)',
                 description="A test model to echo the message property sent in input.",
                 developer='Credmark',
-                input=EchoDto,
-                output=EchoDto)
+                input=EchoExampleInput,
+                output=EchoExampleOutput)
 class EchoModel(Model):
-    def run(self, input: EchoDto) -> EchoDto:
-        return input
+    def run(self, input: EchoExampleInput) -> EchoExampleOutput:
+        output = EchoExampleOutput(
+            github_url="https://github.com/credmark/credmark-models-py/blob/main/models/examples/e_01_model.py",
+            documentation_url="https://developer-docs.credmark.com/en/latest/components.html#model-class",
+            echo=input.message
+        )
+
+        output.log("This is a basic model")
+        output.log("You can supply a message (str) as input with default value of Hello")
+        output.log_io(input="input.message", output=input.message)
+
+        output.log("It echoes back the input message")
+        return output
