6b48008158939887e66ae0e48a2af389d7f52413
Vineet <vineet@altercode.xyz>
2022-04-26 14:04:08 +0530

    Updated error example

--- models/examples/e_13_error.py
+++ models/examples/e_13_error.py
@@ -3,37 +3,30 @@ from credmark.cmf.model.errors import ModelDataError
 
 
 @Model.describe(
-    slug='example.data-error',
+    slug='example.data-error-1',
     version='1.0',
-    display_name='Data Error Example',
-    description="A test model to generate a ModelDataError.",
+    display_name='Example - Data Error 1',
+    description="An example model to generate a ModelDataError.",
     developer='Credmark',
     input=EmptyInput,
     errors=ModelDataErrorDesc(code=ModelDataError.Codes.NO_DATA,
                               code_desc='Data does not exist'))
-class Model1(Model):
-    """
-    """
-
-    def run(self, input: dict) -> dict:
+class ExampleDataError1(Model):
+    def run(self, _) -> dict:
         raise ModelDataError('Data does not exist', ModelDataError.Codes.NO_DATA)
 
 
 @Model.describe(
     slug='example.data-error-2',
     version='1.0',
-    display_name='Data Error 2 Example',
-    description="A test model to generate a ModelDataError with 2 codes defined.",
+    display_name='Example - Data Error 2',
+    description="An example model to generate a ModelDataError with 2 codes defined.",
     developer='Credmark',
     input=EmptyInput,
-    errors=ModelDataErrorDesc(codes=[(ModelDataError.Codes.NO_DATA,
-                                      'Data does not exist'),
-                                     (ModelDataError.Codes.CONFLICT,
-                                      'Conflicting values in input')])
+    errors=ModelDataErrorDesc(
+        codes=[(ModelDataError.Codes.NO_DATA, 'Data does not exist'),
+               (ModelDataError.Codes.CONFLICT, 'Conflicting values in input')])
 )
-class Model2(Model):
-    """
-    """
-
-    def run(self, input: dict) -> dict:
-        raise ModelDataError('Data does not exist', ModelDataError.Codes.NO_DATA)
+class ExampleDataError2(Model):
+    def run(self, _) -> dict:
+        raise ModelDataError('Conflicting values in input', ModelDataError.Codes.CONFLICT)
