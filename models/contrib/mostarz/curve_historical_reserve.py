69e24f82a71fff952879c85896faa7964be23626
Momin Ahmad <momin@credmark.com>
2022-04-27 17:20:29 -0500

    Update curve_historical_reserve.py

--- models/contrib/mostarz/curve_historical_reserve.py
+++ models/contrib/mostarz/curve_historical_reserve.py
@@ -8,7 +8,8 @@ from credmark.cmf.types import (
 @Model.describe(slug="contrib.curve-fi-pool-historical-reserve",
                 version="1.1",
                 display_name="Curve Finance Pool Liqudity",
-                description="gets reserver ratio of stablecoin pools in Curve for every day in the past year",
+                description="gets reserve ratio of stablecoin"
+                "pools in Curve for every day in the past year",
                 input=Contract,
                 output=dict)
 class CurveFinanceHistoricalReserve(Model):
