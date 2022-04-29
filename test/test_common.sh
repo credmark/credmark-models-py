8ce5aad222ca98e3f24ad19e73fee94a4ddd7523
kunlun <leafyoung@gmail.com>
2022-04-29 16:47:14 +0800

    update test

--- test/test_common.sh
+++ test/test_common.sh
@@ -196,6 +196,5 @@ echo_cmd () {
     fi
 }
 
-
-token_price_deps='token.price,token.price-ext,uniswap-v2.get-average-price,uniswap-v3.get-average-price,sushiswap.get-average-price'
+token_price_deps='token.price,token.price-ext,uniswap-v2.get-average-price,uniswap-v3.get-average-price,sushiswap.get-average-price,uniswap-v3.get-pool-info'
 var_deps=finance.var-engine,finance.var-reference,token.price-ext,finance.get-one,${token_price_deps}
