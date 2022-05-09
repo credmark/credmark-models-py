fab5ac100a4b1e5d926308264d2d0a9669929f11
kunlun <leafyoung@gmail.com>
2022-05-09 20:18:25 +1000

    update doc

--- models/credmark/algorithms/minimum_risk.py
+++ models/credmark/algorithms/minimum_risk.py
@@ -13,6 +13,10 @@ from models.credmark.protocols.lending.compound.compound_v2 import CompoundV2Poo
                 input=EmptyInput,
                 output=dict)
 class Minrisk(Model):
+    """
+    Doc is
+        https://docs.credmark.com/smart-money-in-defi/investment-concepts/minimum-risk-rate-of-defi
+    """
     STABLECOINS = {
         "USDT": '0xdac17f958d2ee523a2206206994597c13d831ec7',
         "USDC": '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
