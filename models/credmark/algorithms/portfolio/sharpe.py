d12b6b21b439d57c96d8baf78b1132f0764651d7
kunlun <leafyoung@gmail.com>
2022-05-10 11:50:06 +1000

    update input to be generic

--- models/credmark/algorithms/portfolio/sharpe.py
+++ models/credmark/algorithms/portfolio/sharpe.py
@@ -9,14 +9,14 @@ import pandas as pd
 
 class SharpRatioInput(DTO):
     token: Token
-    return_rolling_days: int = DTOField(182, ge=2)
+    window: str = DTOField("360 days")
     risk_free_rate: float
 
 
 @Model.describe(slug="finance.sharpe-ratio-token",
                 version="1.0",
                 display_name="Sharpe ratio for a token's historical price performance",
-                description=("Sharpe ratio is return (averaged daily returns, annualized) "
+                description=("Sharpe ratio is return (averaged returns, annualized) "
                              "versus risk (std. dev. of return)"),
                 input=SharpRatioInput,
                 output=dict)
@@ -41,14 +41,12 @@ class SharpeRatioToken(Model):
 
     def run(self, input: SharpRatioInput) -> dict:
         token = input.token
-        return_rolling_days = input.return_rolling_days
-        window = f'{int(input.return_rolling_days*2)} days'
+        window = input.window
         risk_free_rate = input.risk_free_rate
 
         pl = self.context.historical.run_model_historical('token.price',
                                                           model_input=token,
                                                           window=window,
-                                                          interval='1 day',
                                                           model_return_type=Price)
 
         df_pl = pd.DataFrame(pl.dict()['series']).sort_values(['blockNumber'], ascending=False)
@@ -57,22 +55,28 @@ class SharpeRatioToken(Model):
         df_pl = (df_pl.loc[:, ['blockNumber', 'blockTimestamp', 'blockTime', 'price']]
                  .reset_index(drop=True))
 
+        # If the number of rows is odd, we remove the last row (data in descending).
+        if df_pl.shape[0] & 0x1:
+            df_pl = df_pl[:-1]
+        return_rolling_interval = int(df_pl.shape[0]/2)
+
         np_historical_prices = df_pl.price.to_numpy()
         daily_return = np_historical_prices[:-1] / np_historical_prices[1:] - 1
 
         annualized_return = daily_return * np.sqrt(365)
         avg_6m_ret = (pd.Series(annualized_return)
-                      .rolling(return_rolling_days)
-                      .mean()[return_rolling_days-1:])
+                      .rolling(return_rolling_interval)
+                      .mean()[return_rolling_interval-1:])
 
-        st_dev = (avg_6m_ret.rolling(return_rolling_days)
+        st_dev = (avg_6m_ret.rolling(return_rolling_interval)
                   .std()
-                  [(return_rolling_days-1):])
+                  [(return_rolling_interval-1):])
 
-        sharpe_ratio = (avg_6m_ret[return_rolling_days-1:] - risk_free_rate) / st_dev
+        sharpe_ratio = (avg_6m_ret[return_rolling_interval-1:] - risk_free_rate) / st_dev
 
         ret_dict = {"sharpe_ratio": sharpe_ratio.to_list()[0],
                     'window': window,
+                    'return_rolling_interval': return_rolling_interval,
                     'blockTime': str(df_pl.blockTime[0]),
                     'block_number': int(df_pl.blockNumber[0]),
                     'blockTimestamp': int(df_pl.blockTimestamp[0]),
