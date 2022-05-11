ffd8a0ed7a1afec1833e44b352491ee3c1337cef
kunlun <leafyoung@gmail.com>
2022-05-11 10:55:12 +1000

    remove temp file

--- models/contrib/kunlun/token.py
+++
@@ -1,8 +0,0 @@
-
-@Model.describe(slug='console.token-total-supply',
-                version='1.0',
-                input=Token,
-                output=dict)
-class ConsoleTokenSupply(Model):
-    def run(self, input: Token) -> dict:
-        return {'total_supply': input.token.scaled(input.token.total_supply)}
