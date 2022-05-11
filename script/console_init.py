8cd32f4177c2a1a28a9c8589b2e9773037cebc0a
kunlun <leafyoung@gmail.com>
2022-05-11 22:55:45 +1000

    use importlib

--- script/console_init.py
+++ script/console_init.py
@@ -1,29 +1,28 @@
-import models.tmp_abi_lookup as abi_lookup
-
-from models.credmark.protocols.lending.aave.aave_v2 import (
-    AaveDebtInfo,
-    AaveDebtInfos,
-)
-
-from models.credmark.protocols.lending.compound.compound_v2 import (
-    CompoundV2PoolInfo,
-    CompoundV2PoolValue
-)
-
-from models.credmark.protocols.dexes.curve.curve_finance import (
-    CurveFiPoolInfo,
-    CurveFiPoolInfos,
-)
-
-from models.dtos.volume import (
-    TradingVolume,
-    TokenTradingVolume,
-)
-
-from models.dtos.price import (
-    PoolPriceInfo,
-    PoolPriceInfos,
-    PoolPriceAggregatorInput,
-)
+import importlib
 
 
+def load_modules(environ):
+    environ['abi_lookup'] = importlib.import_module('models.tmp_abi_lookup')
+
+    aave_v2 = importlib.import_module('models.credmark.protocols.lending.aave.aave_v2')
+    environ['AaveDebtInfo'] = aave_v2.AaveDebtInfo
+    environ['AaveDebtInfos'] = aave_v2.AaveDebtInfos
+
+    compound_v2 = importlib.import_module('models.credmark.protocols.lending.compound.compound_v2')
+    environ['CompoundV2PoolInfo'] = compound_v2.CompoundV2PoolInfo
+    environ['CompoundV2PoolValue'] = compound_v2.CompoundV2PoolValue
+
+    curve_finance = importlib.import_module('models.credmark.protocols.dexes.curve.curve_finance')
+    environ['CurveFiPoolInfo'] = curve_finance.CurveFiPoolInfo
+    environ['CurveFiPoolInfos'] = curve_finance.CurveFiPoolInfos
+
+    dto_volume = importlib.import_module('models.dtos.volume')
+    environ['TradingVolume'] = dto_volume.TradingVolume
+    environ['TokenTradingVolume'] = dto_volume.TokenTradingVolume
+
+    dto_prices = importlib.import_module('models.dtos.price')
+    environ['PoolPriceInfo'] = dto_prices.PoolPriceInfo
+    environ['PoolPriceInfos'] = dto_prices.PoolPriceInfos
+    environ['PoolPriceAggregatorInput'] = dto_prices.PoolPriceAggregatorInput
+
+    print(environ.keys())
