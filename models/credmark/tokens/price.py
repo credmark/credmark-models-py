4ba8b116664fdd437885a74ca8ea3797d259087c
iwyrkore <iwyrkore@gmail.com>
2022-05-14 10:15:25 -0400

    Added ModelDataErrorDesc for price models.

--- models/credmark/tokens/price.py
+++ models/credmark/tokens/price.py
@@ -2,7 +2,7 @@
 import pandas as pd
 from typing import List
 
-from credmark.cmf.model import Model
+from credmark.cmf.model import Model, ModelDataErrorDesc
 from credmark.cmf.model.errors import ModelDataError
 from credmark.cmf.types import (
     Token,
@@ -32,12 +32,18 @@ class PriceModel(Model):
         return self.context.run_model('token.price', input, return_type=Price)
 
 
+PRICE_DATA_ERROR_DESC = ModelDataErrorDesc(
+    code=ModelDataError.Codes.NO_DATA,
+    code_desc='No pools to aggregate for token price')
+
+
 @Model.describe(slug='price.pool-aggregator',
                 version='1.1',
                 display_name='Token Price from DEX pools, weighted by liquidity',
                 description='Aggregate prices from pools weighted by liquidity',
                 input=PoolPriceAggregatorInput,
-                output=Price)
+                output=Price,
+                errors=PRICE_DATA_ERROR_DESC)
 class PoolPriceAggregator(Model):
     def run(self, input: PoolPriceAggregatorInput) -> Price:
         if len(input.pool_price_infos) == 0:
@@ -64,7 +70,8 @@ class PriceWeight:
                 display_name='Uniswap v3 - get price weighted by liquidity',
                 description='The Uniswap v3 pools that support a token contract',
                 input=Token,
-                output=Price)
+                output=Price,
+                errors=PRICE_DATA_ERROR_DESC)
 class UniswapV3GetAveragePrice(Model, PriceWeight):
     def run(self, input: Token) -> Price:
         pool_price_infos = self.context.run_model('uniswap-v3.get-pool-price-info',
@@ -83,7 +90,8 @@ class UniswapV3GetAveragePrice(Model, PriceWeight):
                 display_name='Uniswap v2 - get price weighted by liquidity',
                 description='The Uniswap v2 pools that support a token contract',
                 input=Token,
-                output=Price)
+                output=Price,
+                errors=PRICE_DATA_ERROR_DESC)
 class UniswapV2GetAveragePrice(Model, PriceWeight):
     def run(self, input: Token) -> Price:
         pool_price_infos = self.context.run_model('uniswap-v2.get-pool-price-info',
@@ -102,7 +110,8 @@ class UniswapV2GetAveragePrice(Model, PriceWeight):
                 display_name='Sushi v2 (Uniswap V2) - get price weighted by liquidity',
                 description='The Sushi v2 pools that support a token contract',
                 input=Token,
-                output=Price)
+                output=Price,
+                errors=PRICE_DATA_ERROR_DESC)
 class SushiV2GetAveragePrice(Model, PriceWeight):
     def run(self, input: Token) -> Price:
         pool_price_infos = self.context.run_model('sushiswap.get-pool-price-info',
@@ -144,7 +153,8 @@ class TokenPoolPriceInfo(Model):
                  description='The Current Credmark Supported Price Algorithm',
                  developer='Credmark',
                  input=Token,
-                 output=Price)
+                 output=Price,
+                 errors=PRICE_DATA_ERROR_DESC)
 class TokenPriceModel(Model, PriceWeight):
     """
     Return token's price
