814338e287e224f61aa433bc2aa5feff0945c8c9
kunlun <leafyoung@gmail.com>
2022-05-17 19:13:33 +0800

    update to use feed registry

--- models/credmark/protocols/oracle/chainlink.py
+++ models/credmark/protocols/oracle/chainlink.py
@@ -1,47 +1,49 @@
 from credmark.cmf.model import Model
-from credmark.cmf.types import Contract, Price
-from credmark.dto import  EmptyInput
+from credmark.cmf.types import Contract, Price, Token, Address
+from credmark.dto import EmptyInput
 
 
-@Model.describe(slug="chainlink.eth-usd",
+@Model.describe(slug='chainlink.get-feed-registry',
                 version="1.0",
-                display_name="Chainlink - ETH / USD",
-                description="Chainlink - ETH / USD",
+                display_name="Chainlink - Feed registry",
+                description="Supports multi-chain",
                 input=EmptyInput,
-                output=Price)
-class ChainLinkETHUSD(Model):
-    CHAINLINK_ETH_USD = {
-        1: '0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419',
-        4: '0x8A753747A1Fa494EC906cE90E9f37563A8AF630e',  # Rinkeby
-        42: '0x9326BFA02ADD2366b30bacB125260Af641031331',  # Kovan
+                output=Contract)
+class ChainLinkFeedRegistry(Model):
+    CHAINLINK_REGISTRY = {
+        1: '0x47Fb2585D2C56Fe188D0E6ec628a38b74fCeeeDf',
+        42: '0xAa7F6f7f507457a1EE157fE97F6c7DB2BEec5cD0'
     }
 
-    def run(self, _) -> Price:
-        oracle = Contract(address=self.CHAINLINK_ETH_USD[self.context.chain_id])
-        (_roundId, answer,
-         _startedAt, _updatedAt, _answeredInRound) = oracle.functions.latestRoundData().call()
-        decimals = oracle.functions.decimals().call()
-        return Price(price=answer / (10 ** decimals),
-                     src=f'{self.slug}')
+    def run(self, _) -> Contract:
+        return Contract(address=self.CHAINLINK_REGISTRY[self.context.chain_id])
 
 
-@Model.describe(slug="chainlink.btc-usd",
+@Model.describe(slug='chainlink.price-usd',
                 version="1.0",
-                display_name="Chainlink - BTC / USD",
-                description="Chainlink - BTC / USD",
-                input=EmptyInput,
+                display_name="Chainlink - Price for Token / USD pair",
+                description="",
+                input=Token,
                 output=Price)
-class ChainLinkBTCUSD(Model):
-    CHAINLINK_BTC_USD = {
-        1: '0xF4030086522a5bEEa4988F8cA5B36dbC97BeE88c',
-        4: '0xECe365B379E1dD183B20fc5f022230C044d51404',  # Rinkeby
-        42: '0x6135b13325bfC4B00278B4abC5e20bbce2D6580e',  # Kovan
-    }
+class ChainLinkFeedPrice(Model):
+    ETH = Address('0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE')
+    BTC = Address('0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB')
 
-    def run(self, _) -> Price:
-        oracle = Contract(address=self.CHAINLINK_BTC_USD[self.context.chain_id])
+    USD = Address('0x{:040x}'.format(840))
+    GBP = Address('0x{:040x}'.format(826))
+    EUR = Address('0x{:040x}'.format(978))
+
+    def run(self, input) -> Contract:
+        registry = Contract(**self.context.models.chainlink.get_feed_registry())
         (_roundId, answer,
-         _startedAt, _updatedAt, _answeredInRound) = oracle.functions.latestRoundData().call()
-        decimals = oracle.functions.decimals().call()
+         _startedAt, _updatedAt,
+         _answeredInRound) = registry.functions.latestRoundData(input.address, self.USD).call()
+        decimals = registry.functions.decimals(input.address, self.USD).call()
+        description = registry.functions.description(input.address, self.USD).call()
+        version = registry.functions.version(input.address, self.USD).call()
+        feed = registry.functions.getFeed(input.address, self.USD).call()
+        isFeedEnabled = registry.functions.isFeedEnabled(feed).call()
+        time_diff = self.context.block_number.timestamp - _updatedAt
+        round_diff = _answeredInRound - _roundId
         return Price(price=answer / (10 ** decimals),
-                     src=f'{self.slug}')
+                     src=f'{self.slug}|{description}|{feed}|v{version}|{isFeedEnabled}|t:{time_diff}s|r:{round_diff}')
