34836109a97dbe31288abc5c1ad6d20ddf9a1a27
kunlun <kunlun@credmark.com>
2022-06-10 01:09:28 -0500

    add compose test case

--- test/run.sh
+++ test/run.sh
@@ -45,6 +45,7 @@ fi
 source $SCRIPT_DIRECTORY/test_examples.sh
 source $SCRIPT_DIRECTORY/test_account_token.sh
 source $SCRIPT_DIRECTORY/test_chainlink.sh
+source $SCRIPT_DIRECTORY/test_compose.sh
 source $SCRIPT_DIRECTORY/test_cmk.sh
 source $SCRIPT_DIRECTORY/test_tvl.sh
 source $SCRIPT_DIRECTORY/test_aave.sh
