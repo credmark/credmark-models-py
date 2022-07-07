38b65f2f967cd6a6bd307cd1ca6822cb5ccd0294
Yang Ye <kunlun@credmark.com>
2022-07-07 23:26:54 +0800

    split pool-price-info out

--- models/credmark/protocols/dexes/uniswap/uniswap_v3.py
+++ models/credmark/protocols/dexes/uniswap/uniswap_v3.py
@@ -1,3 +1,4 @@
+import IPython.lib.pretty as iptty
 import numpy as np
 from credmark.cmf.model import Model
 from credmark.cmf.model.errors import ModelDataError, ModelRunError
@@ -35,9 +36,13 @@ class UniswapV3PoolInfo(DTO):
     token0_symbol: str
     token1_symbol: str
 
+    def _repr_pretty_(self, p, cycle):
+        ctor = iptty.CallExpression.factory(self.__class__.__name__)
+        p.pretty(ctor(self.address, self.sqrtPriceX96))
+
 
 @Model.describe(slug='uniswap-v3.get-pools',
-                version='1.1',
+                version='1.2',
                 display_name='Uniswap v3 Token Pools',
                 description='The Uniswap v3 pools that support a token contract',
                 category='protocol',
@@ -50,7 +55,7 @@ class UniswapV3GetPoolsForToken(Model):
     }
 
     def run(self, input: Token) -> Contracts:
-        fees = [3000, 10000]
+        fees = [100, 500, 3000, 10000]
         primary_tokens = [Token(symbol='DAI'),
                           Token(symbol='USDT'),
                           Token(symbol='WETH'),
@@ -187,21 +192,90 @@ class UniswapV3GetPoolInfo(Model):
         return UniswapV3PoolInfo(**res)
 
 
+@Model.describe(slug='uniswap-v3.get-pool-price-info',
+                version='0.0',
+                display_name='Uniswap v3 Token Pools Info for Price',
+                description='Extract price information for a UniV3 pool',
+                category='protocol',
+                subcategory='uniswap-v3',
+                input=Contract,
+                output=PoolPriceInfo)
+class UniswapV3GetTokenPoolPriceInfo(Model):
+    def run(self, input: Contract) -> PoolPriceInfo:
+        info = self.context.run_model('uniswap-v3.get-pool-info',
+                                      input=input,
+                                      return_type=UniswapV3PoolInfo)
+
+        weth_price = None
+
+        info.token0 = fix_erc20_token(info.token0)
+        info.token1 = fix_erc20_token(info.token1)
+        # decimal only available for ERC20s
+        if not info.token0.decimals or not info.token1.decimals:
+            raise ModelRunError(
+                'Details on token0 {info.token0.decimals=} or token1 {info.token1.decimals=} are incomplete/')
+
+        scale_multiplier = (10 ** (info.token0.decimals - info.token1.decimals))
+        tick_price = 1.0001 ** info.tick * scale_multiplier
+        _tick_liquidity = info.tick_liquidity_token1
+        virtual_liquidity = info.virtual_liquidity_token1
+        ratio_price = info.sqrtPriceX96 * info.sqrtPriceX96 / (2 ** 192) * scale_multiplier
+
+        inverse = False
+        if input.address == info.token1.address:
+            tick_price = 1/tick_price
+            ratio_price = 1/ratio_price
+            inverse = True
+            _tick_liquidity = info.tick_liquidity_token0
+            virtual_liquidity = info.virtual_liquidity_token0
+
+        weth_multiplier = 1.0
+        weth = Token(symbol='WETH')
+        if input.address != weth.address:
+            if weth.address in (info.token1.address, info.token0.address):
+                if weth_price is None:
+                    weth_price = self.context.run_model('price.quote',
+                                                        {"base": weth},
+                                                        return_type=Price)
+                    if weth_price.price is None:
+                        raise ModelRunError('Can not retriev price for WETH')
+
+                    weth_price = weth_price.price
+                weth_multiplier = weth_price
+
+        tick_price *= weth_multiplier
+        ratio_price *= weth_multiplier
+
+        pool_price_info = PoolPriceInfo(src=self.slug,
+                                        price=tick_price,
+                                        liquidity=virtual_liquidity,
+                                        weth_multiplier=weth_multiplier,
+                                        inverse=inverse,
+                                        token0_address=info.token0.address,
+                                        token1_address=info.token1.address,
+                                        token0_symbol=info.token0.symbol,
+                                        token1_symbol=info.token1.symbol,
+                                        token0_decimals=info.token0.decimals,
+                                        token1_decimals=info.token1.decimals,
+                                        pool_address=info.address)
+        return pool_price_info
+
+
 @Model.describe(slug='uniswap-v3.get-pool-info-token-price',
-                version='1.6',
+                version='1.7',
                 display_name='Uniswap v3 Token Pools Price ',
                 description='Gather price and liquidity information from pools',
                 category='protocol',
                 subcategory='uniswap-v3',
                 input=Token,
                 output=Many[PoolPriceInfo])
-class UniswapV3GetTokenPricePoolInfo(Model):
+class UniswapV3GetTokenPoolInfo(Model):
     def run(self, input: Token) -> Many[PoolPriceInfo]:
         pools = self.context.run_model('uniswap-v3.get-pools',
                                        input,
                                        return_type=Contracts)
 
-        model_slug = 'uniswap-v3.get-pool-info'
+        model_slug = 'uniswap-v3.get-pool-price-info'
         model_inputs = pools.contracts
 
         def _use_compose():
@@ -209,7 +283,7 @@ class UniswapV3GetTokenPricePoolInfo(Model):
                 slug='compose.map-inputs',
                 input={'modelSlug': model_slug,
                        'modelInputs': model_inputs},
-                return_type=MapInputsOutput[dict, UniswapV3PoolInfo])
+                return_type=MapInputsOutput[dict, PoolPriceInfo])
 
             infos = []
             for pool_n, p in enumerate(pool_infos):
@@ -227,62 +301,10 @@ class UniswapV3GetTokenPricePoolInfo(Model):
         def _use_for():
             infos = []
             for minput in model_inputs:
-                pi = self.context.run_model(model_slug, minput, return_type=UniswapV3PoolInfo)
+                pi = self.context.run_model(model_slug, minput, return_type=PoolPriceInfo)
                 infos.append(pi)
             return infos
 
         infos = _use_compose()
 
-        prices_with_info = []
-        weth_price = None
-        for info in infos:
-            info.token0 = fix_erc20_token(info.token0)
-            info.token1 = fix_erc20_token(info.token1)
-            # decimal only available for ERC20s
-            if info.token0.decimals and info.token1.decimals:
-                scale_multiplier = (10 ** (info.token0.decimals - info.token1.decimals))
-                tick_price = 1.0001 ** info.tick * scale_multiplier
-                _tick_liquidity = info.tick_liquidity_token1
-                virtual_liquidity = info.virtual_liquidity_token1
-                ratio_price = info.sqrtPriceX96 * info.sqrtPriceX96 / (2 ** 192) * scale_multiplier
-
-                inverse = False
-                if input.address == info.token1.address:
-                    tick_price = 1/tick_price
-                    ratio_price = 1/ratio_price
-                    inverse = True
-                    _tick_liquidity = info.tick_liquidity_token0
-                    virtual_liquidity = info.virtual_liquidity_token0
-
-                weth_multiplier = 1.0
-                weth = Token(symbol='WETH')
-                if input.address != weth.address:
-                    if weth.address in (info.token1.address, info.token0.address):
-                        if weth_price is None:
-                            weth_price = self.context.run_model('price.quote',
-                                                                {"base": weth},
-                                                                return_type=Price)
-                            if weth_price.price is None:
-                                raise ModelRunError('Can not retriev price for WETH')
-
-                            weth_price = weth_price.price
-                        weth_multiplier = weth_price
-
-                tick_price *= weth_multiplier
-                ratio_price *= weth_multiplier
-
-                pool_price_info = PoolPriceInfo(src=self.slug,
-                                                price=tick_price,
-                                                liquidity=virtual_liquidity,
-                                                weth_multiplier=weth_multiplier,
-                                                inverse=inverse,
-                                                token0_address=info.token0.address,
-                                                token1_address=info.token1.address,
-                                                token0_symbol=info.token0.symbol,
-                                                token1_symbol=info.token1.symbol,
-                                                token0_decimals=info.token0.decimals,
-                                                token1_decimals=info.token1.decimals,
-                                                pool_address=info.address)
-                prices_with_info.append(pool_price_info)
-
-        return Many[PoolPriceInfo](some=prices_with_info)
+        return Many[PoolPriceInfo](some=infos)
