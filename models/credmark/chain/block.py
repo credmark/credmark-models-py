76e4867885eaa196cd0cd941d7839dbf4b9df671
Yang Ye <kunlun@credmark.com>
2022-09-19 20:27:19 +0800

    feat: chain.get-latest-block (non-cacheable) (#206)
    * feat: chain.get-latest-block (non-cacheable)
    
    * update DTO

--- models/credmark/chain/block.py
+++ models/credmark/chain/block.py
@@ -16,3 +16,22 @@ class TimestampOutput(DTO):
 class GetBlockTimestamp(Model):
     def run(self, _) -> TimestampOutput:
         return TimestampOutput(timestamp=self.context.block_number.timestamp)
+
+
+class LatestBlock(DTO):
+    blockNumber: int
+    timestamp: int
+
+
+@Model.describe(slug="chain.get-latest-block",
+                version="0.1",
+                display_name="Obtain latest block",
+                description='block number and timestamp',
+                category='chain',
+                output=LatestBlock)
+class GetLatestBlock(Model):
+    def run(self, _) -> LatestBlock:
+        block_number = self.context.web3.eth.get_block_number()
+        block = self.context.web3.eth.get_block(block_number)
+        block_timestamp = block.timestamp  # type: ignore
+        return LatestBlock(blockNumber=block_number, timestamp=block_timestamp)
