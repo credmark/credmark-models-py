cadfed5cd439c7e732e34591239867a5babeff5b
Yang Ye <kunlun@credmark.com>
2022-10-25 19:08:39 +1100

    Example kunlun (#218)
    * example: token price
    
    * lint

---
+++ models/contrib/kunlun/example_token_price.py
@@ -0,0 +1,39 @@
+from credmark.cmf.model import Model
+from credmark.cmf.types import Price, Some, Token
+
+from models.dtos.price import DexPoolAggregationInput, PoolPriceInfo
+
+
+@Model.describe(slug='contrib.example-token-price',
+                version='1.0',
+                display_name='Token Price - weighted by liquidity',
+                description='The Current Credmark Supported Price Algorithm',
+                developer='Credmark',
+                input=Token,
+                output=Price)
+class TokenPriceModel(Model):
+    """
+    Return token's price
+    """
+
+    WEIGHT_POWER = 4
+
+    def run(self, input: Token) -> Price:
+        all_pool_infos = self.context.run_model('price.dex-pool',
+                                                input=input,
+                                                return_type=Some[PoolPriceInfo])
+
+        # DexPoolAggregationInput is a DTO that embodies
+        # -   token
+        # -   pricing setup, and
+        # -   pool information
+
+        pool_aggregator_input = DexPoolAggregationInput(
+            **all_pool_infos.dict(),
+            **input.dict(),
+            weight_power=self.WEIGHT_POWER,
+            debug=False)
+
+        return self.context.run_model('price.pool-aggregator',
+                                      input=pool_aggregator_input,
+                                      return_type=Price)
