c4fedd793e3f745f7bb5d70e94644cb3d0c1fe01
Yang Ye <kunlun@credmark.com>
2023-01-19 22:18:42 +0800

    enable test output

--- test/test_bsc.py
+++ test/test_bsc.py
@@ -1,4 +1,4 @@
-# pylint:disable=locally-disabled,line-too-long
+# pylint:disable=locally-disabled,line-too-long,unsubscriptable-object,assignment-from-none
 
 from cmf_test import CMFTest
 
@@ -15,7 +15,9 @@ class TestBSC(CMFTest):
     def test(self):
         self.title('BSC')
 
-        self.run_model('price.oracle-chainlink',
-                       {"base": "0xb86abcb37c3a4b64f74f59301aff131a1becc787"}, block_number=23523151, chain_id=56)
+        last_block_output = self.run_model('chain.get-latest-block', {}, block_number=None, chain_id=56)
 
-        self.run_model('chain.get-latest-block', {},  block_number=23523151, chain_id=56)
+        last_block = last_block_output['output']['blockNumber']
+
+        self.run_model('price.oracle-chainlink',
+                       {"base": "0xb86abcb37c3a4b64f74f59301aff131a1becc787"}, block_number=last_block, chain_id=56)
--- test/test_polygon.py
+++ test/test_polygon.py
@@ -1,14 +1,17 @@
-# pylint:disable=locally-disabled,line-too-long
+# pylint:disable=locally-disabled,line-too-long,unsubscriptable-object,assignment-from-none
 
 from cmf_test import CMFTest
 
-
 # credmark-dev run price.oracle-chainlink -i '{"base":"0x1ba42e5193dfa8b03d15dd1b86a3113bbbef8eeb"}' -b 23523151 -c 137 -l "*" -j --api_url=http://localhost:8700
+
+
 class TestPolygon(CMFTest):
     def test(self):
         self.title('Polygon')
 
-        self.run_model('price.oracle-chainlink',
-                       {"base": "0x1ba42e5193dfa8b03d15dd1b86a3113bbbef8eeb"}, block_number=36286847, chain_id=137)
+        last_block_output = self.run_model('chain.get-latest-block', {}, block_number=None, chain_id=137)
 
-        self.run_model('chain.get-latest-block', {}, block_number=36286847, chain_id=137)
+        last_block = last_block_output['output']['blockNumber']
+
+        self.run_model('price.oracle-chainlink',
+                       {"base": "0x1ba42e5193dfa8b03d15dd1b86a3113bbbef8eeb"}, block_number=last_block-100, chain_id=137)
