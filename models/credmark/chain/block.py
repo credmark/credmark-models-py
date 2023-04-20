925d9b6a4bc0e4ee9794096f590e76562fb37f2a
Yang Ye <kunlun@credmark.com>
2023-04-21 03:09:26 +0800

    feat: chain.get-block from timestamp (#311)
    * feat: chain.get-block from timestamp
    
    * lint
    
    * add test

--- models/credmark/chain/block.py
+++ models/credmark/chain/block.py
@@ -1,6 +1,8 @@
 from credmark.cmf.model import Model
 from credmark.dto import DTO, EmptyInput
 
+from credmark.cmf.model.errors import (ModelDataError)
+
 
 class TimestampInput(DTO):
     block_number: int
@@ -22,6 +24,81 @@ class GetBlockTimestamp(Model):
         block_timestamp = self.context.web3.eth.get_block(
             input.block_number)['timestamp']  # type: ignore
         return TimestampOutput(timestamp=block_timestamp)
+
+
+class BlockInput(DTO):
+    timestamp: int
+
+
+class BlockOutput(DTO):
+    block_number: int
+    block_timestamp: int
+    sample_timestamp: int
+
+
+@Model.describe(slug="chain.get-block",
+                version="0.1",
+                display_name="Obtain block from timestamp",
+                description='In UTC',
+                category='chain',
+                input=BlockInput,
+                output=BlockOutput)
+class GetBlock(Model):
+    timestamp_by_block = {}
+
+    def binary_search(self, low, high, timestamp):
+        # Check base case
+        assert high >= low
+        mid = (high + low)//2
+        if high == low:
+            return low, self.timestamp_by_block[low]
+
+        if self.timestamp_by_block.get(mid) is None:
+            mid_time = self.context.web3.eth.get_block(
+                mid)['timestamp']  # type: ignore
+            self.timestamp_by_block[mid] = mid_time
+        else:
+            mid_time = self.timestamp_by_block[mid]
+
+        if mid_time == timestamp:
+            return mid, mid_time
+        elif high - low == 1:
+            return low, self.timestamp_by_block[low]
+        elif mid_time < timestamp:
+            return self.binary_search(mid, high, timestamp)
+        elif mid_time > timestamp:
+            return self.binary_search(low, mid-1, timestamp)
+        else:
+            return -1, -1
+
+    def run(self, input: BlockInput) -> BlockOutput:
+        if self.context.block_number != 0:
+            return self.context.run_model(self.slug, input, block_number=0, return_type=BlockOutput)
+
+        start_time = self.context.run_model(
+            'chain.get-block-timestamp', input={'block_number': 0})
+        end_time = self.context.run_model(
+            'chain.get-latest-block')
+
+        self.timestamp_by_block[0] = start_time['timestamp']
+        self.timestamp_by_block[
+            end_time["blockNumber"]] = end_time["timestamp"]
+
+        if input.timestamp < start_time['timestamp']:
+            raise ModelDataError(
+                f'{input.timestamp=} is before the first block ({start_time["timestamp"]})')
+
+        if input.timestamp > end_time['timestamp']:
+            raise ModelDataError(f'{input.timestamp=} is after the the latest block '
+                                 f'({end_time["blockNumber"]} @ {end_time["timestamp"]})')
+
+        block_number, block_timestamp = self.binary_search(
+            0, end_time['blockNumber'], input.timestamp)
+
+        return BlockOutput(
+            block_number=block_number,
+            block_timestamp=block_timestamp,
+            sample_timestamp=input.timestamp)
 
 
 class LatestBlock(DTO):
