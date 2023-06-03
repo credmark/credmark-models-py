b7d9df67858b4b20b0b445dd98b6690be678a2e6
kunlun <leafyoung@gmail.com>
2023-06-03 14:21:57 +0800

    change univ2 and v3 meta to use_for

--- models/credmark/protocols/dexes/uniswap/uniswap_v2_meta.py
+++ models/credmark/protocols/dexes/uniswap/uniswap_v2_meta.py
@@ -364,5 +364,5 @@ class UniswapV2PoolMeta(Model):
                     infos.append(pi.just)
             return infos
 
-        infos = _use_compose()
+        infos = _use_for(local=False)
         return Some[PoolPriceInfo](some=infos)
