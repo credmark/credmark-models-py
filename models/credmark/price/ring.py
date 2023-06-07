971f4d5f74592887cfd74d59e96f7cf5bd89ba9d
kunlun <leafyoung@gmail.com>
2023-06-07 15:22:27 +0800

    add ignore

--- models/credmark/price/ring.py
+++ models/credmark/price/ring.py
@@ -91,7 +91,7 @@ class DexSecondaryTokens(Model):
     RING1_TOKENS = {
         Network.Mainnet: {
             **{protocol: (lambda block_number:
-                          [Token('WETH')] if block_number <= 17_385_741
+                          [Token('WETH')] if block_number <= 17_385_740
                           else [Token('WETH'), Token('WBTC')])
                for protocol in [DexProtocol.UniswapV2,
                                 DexProtocol.UniswapV3,
