1140fb6f990d6bf2e397369cb5f1ffd856b5f441
kunlun <leafyoung@gmail.com>
2023-06-13 17:55:28 +0800

    uni_pool_base add load_events_etl

--- models/credmark/protocols/dexes/uniswap/uni_pool_base.py
+++ models/credmark/protocols/dexes/uniswap/uni_pool_base.py
@@ -23,6 +23,19 @@ class UniswapPoolBase:
         self._event_list = event_list
         self.protocol = _protocol
 
+    def load_events_etl(self, _chain_id, _pool_addr, _protocol, _start_block, _end_block, pool_id, _get_uniswap_event_etl):
+        """
+        load events from etl
+        """
+        for event_name in self._event_list:
+            # no fix for types, will fix when insertion
+            df_evt = _get_uniswap_event_etl(_chain_id, _pool_addr, _protocol, event_name, _start_block, _end_block, pool_id, _fix_df_events=None)
+            self.df_evt[event_name] = df_evt
+
+        print(('[load_events_etl]', [(k, v.shape[0]) for k, v in self.df_evt.items()]),
+              file=sys.stderr, flush=True)
+
+
     def load_events(self, from_block, to_block, use_async: bool, async_worker: int):
         """
         load events from node
@@ -32,25 +45,16 @@ class UniswapPoolBase:
             raise ValueError(f'Pool abi missing for {pool.address}')
 
         for event_name in self._event_list:
+            # no fix for types, will fix when insertion
             df_evt = fetch_events_with_cols(
                 pool, getattr(pool.events, event_name), event_name,
                 from_block, to_block, getattr(pool.abi.events, event_name).args,
                 use_async, async_worker)
             self.df_evt[event_name] = df_evt
 
-        df_comb_evt = pd.concat(self.df_evt.values())
-
-        if df_comb_evt.empty:
-            return df_comb_evt
-
-        df_comb_evt = df_comb_evt.sort_values(['blockNumber', 'logIndex']).reset_index(drop=True)
-
         print(('[load_events]', [(k, v.shape[0]) for k, v in self.df_evt.items()]),
-              ('_comb_evt', df_comb_evt.shape[0],
-               'block_number', df_comb_evt.blockNumber.nunique()),
               file=sys.stderr, flush=True)
 
-        return df_comb_evt
 
     def load_events_db(self, pool_id, protocol, from_block, to_block, fix_df_events, _get_uniswap_event):
         """
