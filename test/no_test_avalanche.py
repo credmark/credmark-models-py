3ed7400a11d61fae8e6427d18cde48b6928f096d
kunlun <leafyoung@gmail.com>
2023-07-13 20:52:09 +0800

    update test

--- test/no_test_avalanche.py
+++ test/no_test_avalanche.py
@@ -1,3 +1,4 @@
+# pylint: disable=line-too-long, pointless-string-statement
 from cmf_test import CMFTest
 
 # 56: bsc
@@ -30,11 +31,18 @@ class TestAvalanche(CMFTest):
         #     block_number=32537910)
 
         # https://snowtrace.io/tx/0x83b307af18c06818619934b15ffd7083a3601a333bc60508ce42901a8edfb762
-        # Curve 2 for avUSDC
-        # credmark-dev run aave-v2.account-info-reserve -i '{"address": "0x7f90122BF0700F9E7e1F688fe926940E8839F353", "reserve": "0x46A51127C3ce23fb7AB1DE06226147F446e4a857"}' -b 32537910 -c 43114 -j
+        # Curve 2 for USDC.e 0xA7D7079b0FEaD91F3e65f86E8915Cb59c1a4C664
+        # receive avUSDC 0x46A51127C3ce23fb7AB1DE06226147F446e4a857
+
+        """
+        credmark-dev run aave-v2.account-info-reserve \
+        -i '{"address": "0x7f90122BF0700F9E7e1F688fe926940E8839F353", "reserve": "0xA7D7079b0FEaD91F3e65f86E8915Cb59c1a4C664"}' \
+        -b 32537910 -c 43114 -j --api_url http://localhost:8700
+        """
+
         self.run_model(
             'aave-v2.account-info-reserve',
             {"address": "0x7f90122BF0700F9E7e1F688fe926940E8839F353",
-                "reserve": "0x46A51127C3ce23fb7AB1DE06226147F446e4a857"},
+                "reserve": "0xA7D7079b0FEaD91F3e65f86E8915Cb59c1a4C664"},
             chain_id=chain_id,
             block_number=32537910)
