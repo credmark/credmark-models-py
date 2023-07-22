6a845d05183bce272d8b8d07fa939fb190c6a0d3
kunlun <leafyoung@gmail.com>
2023-07-22 21:28:38 +0800

    refactor v2 and update test

--- test/test_compound.py
+++ test/test_compound.py
@@ -1,27 +1,22 @@
 # pylint:disable=locally-disabled,line-too-long
 
+from datetime import datetime, timezone
+
 from cmf_test import CMFTest
 
 
 class TestCompound(CMFTest):
-    def test_1(self):
+    def test_deployment(self):
         self.title('Compound')
-
-        # ${token_price_deps}, ${compound_deps}
-        self.run_model('compound-v2.pool-info',
-                       {"address": "0x70e36f6bf80a52b3b46b3af8e106cc0ed743e8e4"})
-        # ${token_price_deps}, compound-v2.get-comptroller
-        self.run_model('compound-v2.pool-info',
-                       {"address": "0x95b4ef2869ebd94beb4eee400a99824bf5dc325b"})
 
         self.run_model('compound-v2.get-comptroller')
         self.run_model('compound-v2.get-pools')  # compound-v2.pool-info
-        # compound-v2.pool-info, compound-v2.get-pools, ${token_price_deps}
-        self.run_model('compound-v2.all-pools-info')
 
-        # self.run_model('compound-v2.pool-value-historical', {"date_range": ["2021-12-15", "2021-12-18"], "token": {"address":"0x70e36f6bf80a52b3b46b3af8e106cc0ed743e8e4"}}) # ${token_price_deps},compound-v2.get-comptroller,compound-v2.pool-info,compound-v2.pool-value,compound-v2.all-pools-values
-        # self.run_model('compound-v2.pool-value-historical', {"date_range": ["2021-09-15", "2021-09-20"], "token": {"address":"0x70e36f6bf80a52b3b46b3af8e106cc0ed743e8e4"}}) # ${token_price_deps},compound-v2.get-comptroller,compound-v2.pool-info,compound-v2.pool-value,compound-v2.all-pools-values
-        # self.run_model('compound-v2.pool-value-historical', {"date_range": ["2022-01-15", "2022-01-18"], "token": {"address":"0x70e36f6bf80a52b3b46b3af8e106cc0ed743e8e4"}}) # ${token_price_deps},compound-v2.get-comptroller,compound-v2.pool-info,compound-v2.pool-value,compound-v2.all-pools-values
+    def test_pool(self):
+        self.run_model('compound-v2.pool-info',
+                       {"address": "0x70e36f6bf80a52b3b46b3af8e106cc0ed743e8e4"})
+        self.run_model('compound-v2.pool-info',
+                       {"address": "0x95b4ef2869ebd94beb4eee400a99824bf5dc325b"})
 
         self.run_model('compound-v2.pool-value',
                        {"address": "0x70e36f6bf80a52b3b46b3af8e106cc0ed743e8e4"}, block_number=16694809)
@@ -38,8 +33,29 @@ class TestCompound(CMFTest):
         self.run_model('compound-v2.pool-info',
                        {"address": "0x5d3a536e4d6dbd6114cc1ead35777bab948e3643"})
 
-    def test_2(self):
+    def test_portfolio(self):
         self.run_model('compound-v2.all-pools-info', {}, block_number=12770589)
+        self.run_model('compound-v2.all-pools-portfolio', {}, block_number=12770589)
 
-        self.run_model('compound-v2.all-pools-portfolio',
-                       {}, block_number=12770589)
+        self.run_model('compound-v2.all-pools-info')
+        self.run_model('compound-v2.all-pools-value')
+        self.run_model('compound-v2.all-pools-portfolio')
+
+    def test_historical(self):
+        dates = [
+            datetime(2021, 9, 20, tzinfo=timezone.utc),
+            datetime(2021, 12, 18, tzinfo=timezone.utc),
+            datetime(2022, 1, 18, tzinfo=timezone.utc),
+        ]
+
+        for dt in dates:
+            block_number = self.run_model_with_output(
+                'chain.get-block',
+                {'timestamp': int(dt.timestamp())})['output']['block_number']
+
+            self.run_model('historical.run-model',
+                           {'model_slug': 'compound-v2.pool-value',
+                            'window': '5 days',
+                            'interval': '1 days',
+                            'model_input': {'address': '0x70e36f6bf80a52b3b46b3af8e106cc0ed743e8e4'}},
+                           block_number=block_number)
