b8f953c63d53ee8acb9eb3a00581ce60b2d6e492
v1nvn <v1n@outlook.com>
2023-12-28 17:19:14 +0530

    suppress deprecation warning

--- models/examples/e_14_library.py
+++ models/examples/e_14_library.py
@@ -1,14 +1,22 @@
+import warnings
+
 import matplotlib
 import numpy
 import pandas
 import pyarrow
 import scipy
-import statsmodels
 import xlrd
 import xlsxwriter
 from credmark.cmf.model import Model
 
 from .dtos import ExampleLibrariesOutput
+
+# TODO: Replace statsmodels library.
+# This library is deprecated. Suppressing deprecation warning until
+# a replacement is found.
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore", category=DeprecationWarning)
+    import statsmodels
 
 
 @Model.describe(
