88f6984fc278e41573099dade6df5d2ed144ade5
Momin Ahmad <mominahmad93@gmail.com>
2024-01-15 11:32:17 -0600

    progress

--- models/contrib/credit-coop/line-of-credit.py
+++ models/contrib/credit-coop/line-of-credit.py
@@ -2,7 +2,7 @@ from credmark.cmf.model import Model
 from credmark.cmf.ipython import create_cmf
 from credmark.cmf.types import Address, BlockNumber, Token, Contract 
 from credmark.dto import DTO
-
+import datetime
 # credmark-dev run <model-name> -i {"contract_address":"address"} -j -c 1 -b latest
 
 
@@ -21,18 +21,38 @@ class LocInput(DTO):
 )
 class LineOfCredit(Model):
     def run(self, input: LocInput) -> dict:
-        result = {}
-        cmf_param = {
-            'chain_id': 1,
-        }
-        context, _model_loader = create_cmf(cmf_param)
-        LineOfCredit = Contract(LocInput.lineOfCreditAddress)
-        [interstAccrued, avaialbleCredit, deadline, normalDeadline] = context.web3_batch.call([
+        
+     
+        LineOfCredit = Contract(input.lineOfCreditAddress)
+        escrow = LineOfCredit.functions.escrow().call()
+        escrowContract = Contract(escrow)
+        id = LineOfCredit.functions.ids(0)
+        
+
+        [interstAccrued, avaialbleCredit, deadline, collateral, credit, token_price] = self.context.web3_batch.call([
             LineOfCredit.functions.interestAccrued(id),
             LineOfCredit.functions.available(id),
             LineOfCredit.functions.deadline(),
-            LineOfCredit.functions.arbiter()
+            escrowContract.functions.collateral(),
+            LineOfCredit.functions.credits(id)
         ], unwrap=True)
 
-        return result
+        normalDeadline = datetime.datetime.fromtimestamp(deadline, datetime.UTC)
+        token = Token(credit[5])
+
+        token_price = self.context.run_model(
+                    slug='price.quote',
+                    input={'base': token})['price']
+
+
+        return {
+            'interestAccrued': (interstAccrued / 10**credit[4]) * token_price,
+            'princeipal outstanding': (credit[1] / 10**credit[4]) * token_price,
+            'outsanding debt total': ((credit[1] + interstAccrued) / 10**credit[4]) * token_price,
+            'available to drawdown': (avaialbleCredit[0] / 10**credit[4]) * token_price,
+            'interest to withdraw': (avaialbleCredit[1]/(10**credit[4])) * token_price,
+            'deadline': normalDeadline,
+            'collateral': collateral,
+        }
+    
 
